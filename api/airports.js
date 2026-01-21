e943cef41de99f64f2e15bcfa0f7fe439fb095a3
Mael Gruand <maelg396@gmail.com>
2026-01-21 13:27:11 +0100

    correcting error top use router

--- api/airports.js
+++ api/airports.js
@@ -1,8 +1,23 @@
+import express from "express"
 import fs from "fs"
 import path from "path"
+
+const router = express.Router()
 
 const __dirname = new URL(".", import.meta.url).pathname
 const dataPath = path.join(__dirname, "../data/airports.json")
 const airports = JSON.parse(fs.readFileSync(dataPath, "utf-8"))
 
-export default airports
+router.get("/", (req, res) => {
+  res.json(airports)
+})
+
+router.get("/:id", (req, res) => {
+  const airport = airports.find(a => a.id === req.params.id)
+  if (!airport) {
+    return res.status(404).json({ error: "Airport not found" })
+  }
+  res.json(airport)
+})
+
+export default router
