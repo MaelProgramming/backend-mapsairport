16e4e43f0e335cf75985bdf69814004c90a8ded0
Mael Gruand <maelg396@gmail.com>
2026-01-21 13:23:41 +0100

    Adding ´export default´ to app

--- api/airports.js
+++ api/airports.js
@@ -4,3 +4,5 @@ import path from "path"
 const __dirname = new URL(".", import.meta.url).pathname
 const dataPath = path.join(__dirname, "../data/airports.json")
 const airports = JSON.parse(fs.readFileSync(dataPath, "utf-8"))
+
+export default airports
