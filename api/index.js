104e413d0233ab2bfc599821b0b0d3a52d732e5a
Mael Gruand <maelg396@gmail.com>
2026-01-21 13:21:11 +0100

    Correct error to minimal code working

--- api/index.js
+++ api/index.js
@@ -2,9 +2,6 @@ import express from "express"
 import cors from "cors"
 
 import airports from "./airports.js"
-import terminals from "./terminals.js"
-import floors from "./floors.js"
-import pois from "./pois.js"
 
 const app = express()
 
@@ -12,9 +9,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use("/airports", airports)
-app.use("/terminals", terminals)
-app.use("/floors", floors)
-app.use("/pois", pois)
+
 
 app.get("/", (req, res) => {
   res.json({ status: "Maps Airport API online ✈️" })
