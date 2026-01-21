8c2c0ab6d18374aa597ab789c0dd53068d93597d
Mael Gruand <maelg396@gmail.com>
2026-01-21 13:19:09 +0100

    addinmg airportss and correcting code

--- api/airports.js
+++ api/airports.js
@@ -1,18 +1,6 @@
-import express from "express"
-import airports from "../data/airports.json" assert { type: "json" }
+import fs from "fs"
+import path from "path"
 
-const router = express.Router()
-
-router.get("/", (req, res) => {
-  res.json(airports)
-})
-
-router.get("/:id", (req, res) => {
-  const airport = airports.find(a => a.id === req.params.id)
-  if (!airport) {
-    return res.status(404).json({ error: "Airport not found" })
-  }
-  res.json(airport)
-})
-
-export default router
+const __dirname = new URL(".", import.meta.url).pathname
+const dataPath = path.join(__dirname, "../data/airports.json")
+const airports = JSON.parse(fs.readFileSync(dataPath, "utf-8"))
