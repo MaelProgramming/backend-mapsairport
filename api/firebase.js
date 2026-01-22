487c4a3c9e0c6ec9d30632724c1b77056826c49e
Mael Gruand <103308970+MaelProgramming@users.noreply.github.com>
2026-01-22 19:57:21 +0100

    Adding '/n' to firebase.js

--- api/firebase.js
+++ api/firebase.js
@@ -2,10 +2,12 @@ import admin from "firebase-admin";
 
 const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_SECRET);
 
+// 🔑 Remplacer les \n par de vrais sauts de ligne
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
 const db = admin.firestore();
-
 export { db };
