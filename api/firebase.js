57fb5bf191eb710d8119da106bb47eb5addd1a9e
Mael Gruand <maelg396@gmail.com>
2026-01-23 12:13:15 +0100

    Correcting error due to file and keys inits

--- api/firebase.js
+++ api/firebase.js
@@ -1,26 +1,13 @@
-import admin from "firebase-admin";
+import admin from 'firebase-admin'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-if (!admin.apps.length) {
-  try {
-    // 1. On nettoie la clé de TOUT caractère parasite (guillemets, espaces, doubles backslashes)
-    const rawKey = process.env.FIREBASE_KEY || "";
-    const formattedKey = rawKey
-      .replace(/\\n/g, '\n') // Change les \n texte en vrais sauts de ligne
-      .trim()                // Enlève les espaces au début/fin
-      .replace(/^"+|"+$/g, ''); // Enlève les guillemets s'ils ont été collés par erreur
 
-    admin.initializeApp({
-      credential: admin.credential.cert({
-        projectId: process.env.PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: formattedKey,
-      }),
-    });
-    
-    console.log("Tentative de connexion avec l'email:", process.env.FIREBASE_CLIENT_EMAIL);
-  } catch (error) {
-    console.error("Erreur setup admin:", error.message);
-  }
-}
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const serviceAccount = path.join(__dirname, "../mapsairport-2025-firebase-adminsdk-fbsvc-0caf1a9bc4.json")
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+})
 
-export const db = admin.firestore();
+const db = admin.firestore()
+export { db }
