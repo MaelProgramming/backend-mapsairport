23986c3fa38a50b9fe0d679651c1b115c0acfb75
Mael Gruand <maelg396@gmail.com>
2026-01-23 13:09:52 +0100

    Correct errors of import unkown

--- api/index.js
+++ api/index.js
@@ -2,8 +2,6 @@ import express from "express"
 import cors from "cors"
 
 import airports from "./airports.js"
-import terminal from "./terminal.js"
-import shops from "./shops.js"
 
 const app = express()
 
@@ -11,8 +9,6 @@ app.use(cors())
 app.use(express.json())
 
 app.use("/airports", airports)
-app.use("/terminal", terminal)
-app.use("/shops", shops)
 app.get("/health", (req, res) => {
   res.json({ ok: true })
 })
