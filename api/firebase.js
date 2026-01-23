c8c4bfa9ae8b5c3cd28313574456d163b23dda41
Mael Gruand <maelg396@gmail.com>
2026-01-23 13:20:45 +0100

    correcting

--- api/firebase.js
+++ api/firebase.js
@@ -3,7 +3,7 @@ import admin from "firebase-admin";
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
+      projectId: process.env.PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       // Décodage de la clé Base64 en string PEM
       privateKey: Buffer.from(process.env.FIREBASE_KEY_BASE64, "base64").toString("utf8"),
